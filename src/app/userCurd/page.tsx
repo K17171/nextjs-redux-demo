5ca76f44f819bcba24e2799ca843c758b177e309
kuldeep prajapat <kuldeepprajapat7@gmail.com>
2024-12-07 09:13:26 +0000

    add new button

--- src/app/userCurd/page.tsx
+++ src/app/userCurd/page.tsx
@@ -12,18 +12,20 @@ export default function Page() {
 
   return (
     <div className="p-6 space-y-4">
-      <button
-        onClick={() => {
-          setEditUser(null);
-          setShowForm(true);
-        }}
-        className="px-6 py-3 bg-green-500 text-white rounded-lg"
-      >
-        Add User
-      </button>
-      <Link href="/" className="px-6 py-3 bg-blue-500 text-lg font-medium rounded-lg hover:bg-blue-600 transition">
-        Back to Home
-      </Link>
+      <div className="flex space-x-4">
+        <button
+          onClick={() => {
+            setEditUser(null);
+            setShowForm(true);
+          }}
+          className="px-6 py-3 bg-green-500 text-white rounded-lg"
+        >
+          Add User
+        </button>
+        <Link href="/" className="px-6 py-3 bg-blue-500 text-lg font-medium rounded-lg hover:bg-blue-600 transition">
+          Back to Home
+        </Link>
+      </div>
       <UserTable
         onEdit={(user) => {
           setEditUser(user);
